1dde657943e0c33055b449d555560578ef5b542f
Dima Usov <dmitry.usov@yahoo.com>
2024-11-08 15:50:04 +0400

    feat: add artist

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MyDbModule } from './mydb/mydb.module';
 import { UserModule } from './user/user.module';
+import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [MyDbModule, UserModule],
+  imports: [MyDbModule, UserModule, TrackModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/mydb/mydb.service.ts
+++ src/mydb/mydb.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ArtistDB } from 'src/in-memory-db/artist.db';
 import { UserDB } from 'src/in-memory-db/user.db';
 
 @Injectable()
 export class MyDBService {
   user = new UserDB();
+  artist = new ArtistDB();
 }
